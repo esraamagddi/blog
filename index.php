5c855c10285827e68ee3972d172fae73d9460200
esraamagddi <esraamagdy20133@gmail.com>
2023-08-15 01:27:17 +0300

    upload insert changes

--- index.php
+++ index.php
@@ -26,6 +26,27 @@
     </div>
     <!-- Banner Ends Here -->
 
+    <?php
+    require_once 'inc/dbconn.php';
+
+    $query="select id, title, image, created_at from posts";
+
+    $runquery=mysqli_query($conn,$query);
+
+
+    if (mysqli_num_rows($runquery)>0)
+    {
+      $posts=mysqli_fetch_all($runquery,MYSQLI_ASSOC);
+
+    }
+    else
+    {
+      $msg= "no posts founded";
+    }
+    
+    
+    ?>
+
     <div class="latest-products">
       <div class="container">
         <div class="row">
@@ -35,134 +56,34 @@
               <!-- <a href="products.html">view all products <i class="fa fa-angle-right"></i></a> -->
             </div>
           </div>
+          <?php
+            if(!empty($posts)):
+          ?>
+          <?php
+          foreach($posts as $post):
+          ?>
           <div class="col-md-4">
             <div class="product-item">
-              <a href="#"><img src="assets/images/product_01.jpg" alt=""></a>
+              <a href="#"><img src="assets/images/postImage/<?= $post['image']?>" alt=""></a>
               <div class="down-content">
-                <a href="#"><h4>Tittle goes here</h4></a>
-                <h6>created_at</h6>
-                <p> Lorem ipsume dolor sit amet, adipisicing elite. Itaque, corporis nulla aspernatur.</p>
-                <!-- <ul class="stars">
-                  <li><i class="fa fa-star"></i></li>
-                  <li><i class="fa fa-star"></i></li>
-                  <li><i class="fa fa-star"></i></li>
-                  <li><i class="fa fa-star"></i></li>
-                  <li><i class="fa fa-star"></i></li>
-                </ul>
-                <span>Reviews (24)</span> -->
+                <a href="#"><h4><?= $post['title']?></h4></a>
+                <h6><?= $post['created_at']?></h6>
+                <p><?php // $post['desc'] loream ?> </p>
+
                 <div class="d-flex justify-content-end">
-                  <a href="viewPost.php" class="btn btn-info "> view</a>
+                  <a href="viewPost.php?id=<?=$post['id'];?>" class="btn btn-info "> view</a>
                 </div>
                 
               </div>
             </div>
           </div>
-          <div class="col-md-4">
-            <div class="product-item">
-              <a href="#"><img src="assets/images/product_02.jpg" alt=""></a>
-              <div class="down-content">
-                <a href="#"><h4>Tittle goes here</h4></a>
-                <h6>created at</h6>
-                <p>Lorem ipsume dolor sit amet, adipisicing elite. Itaque, corporis nulla aspernatur.</p>
-                <!-- <ul class="stars">
-                  <li><i class="fa fa-star"></i></li>
-                  <li><i class="fa fa-star"></i></li>
-                  <li><i class="fa fa-star"></i></li>
-                  <li><i class="fa fa-star"></i></li>
-                  <li><i class="fa fa-star"></i></li>
-                </ul>
-                <span>Reviews (21)</span> -->
-                <div class="d-flex justify-content-end">
-                  <a href="http://" class="btn btn-info "> view</a>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-4">
-            <div class="product-item">
-              <a href="#"><img src="assets/images/product_03.jpg" alt=""></a>
-              <div class="down-content">
-                <a href="#"><h4>Tittle goes here</h4></a>
-                <h6>created_at</h6>
-                <p>Sixteen Clothing is free CSS template provided by TemplateMo.</p>
-                <!-- <ul class="stars">
-                  <li><i class="fa fa-star"></i></li>
-                  <li><i class="fa fa-star"></i></li>
-                  <li><i class="fa fa-star"></i></li>
-                  <li><i class="fa fa-star"></i></li>
-                  <li><i class="fa fa-star"></i></li>
-                </ul>
-                <span>Reviews (36)</span> -->
-                <div class="d-flex justify-content-end">
-                  <a href="http://" class="btn btn-info "> view</a>
-                </div>
-              </div>
-            </div>
-          </div>
-       
-          <div class="col-md-4">
-            <div class="product-item">
-              <a href="#"><img src="assets/images/product_01.jpg" alt=""></a>
-              <div class="down-content">
-                <a href="#"><h4>Tittle goes here</h4></a>
-                <h6>created_at</h6>
-                <p> Lorem ipsume dolor sit amet, adipisicing elite. Itaque, corporis nulla aspernatur.</p>
-                <!-- <ul class="stars">
-                  <li><i class="fa fa-star"></i></li>
-                  <li><i class="fa fa-star"></i></li>
-                  <li><i class="fa fa-star"></i></li>
-                  <li><i class="fa fa-star"></i></li>
-                  <li><i class="fa fa-star"></i></li>
-                </ul>
-                <span>Reviews (24)</span> -->
-                <div class="d-flex justify-content-end">
-                  <a href="http://" class="btn btn-info "> view</a>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-4">
-            <div class="product-item">
-              <a href="#"><img src="assets/images/product_02.jpg" alt=""></a>
-              <div class="down-content">
-                <a href="#"><h4>Tittle goes here</h4></a>
-                <h6>created at</h6>
-                <p>Lorem ipsume dolor sit amet, adipisicing elite. Itaque, corporis nulla aspernatur.</p>
-                <!-- <ul class="stars">
-                  <li><i class="fa fa-star"></i></li>
-                  <li><i class="fa fa-star"></i></li>
-                  <li><i class="fa fa-star"></i></li>
-                  <li><i class="fa fa-star"></i></li>
-                  <li><i class="fa fa-star"></i></li>
-                </ul>
-                <span>Reviews (21)</span> --> 
-                <div class="d-flex justify-content-end">
-                  <a href="http://" class="btn btn-info "> view</a>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-4">  
-            <div class="product-item">
-              <a href="#"><img src="assets/images/product_03.jpg" alt=""></a>
-              <div class="down-content">
-                <a href="#"><h4>Tittle goes here</h4></a>
-                <h6>created_at</h6>
-                <p>Sixteen Clothing is free CSS template provided by TemplateMo.</p>
-                <!-- <ul class="stars">
-                  <li><i class="fa fa-star"></i></li>
-                  <li><i class="fa fa-star"></i></li>
-                  <li><i class="fa fa-star"></i></li>
-                  <li><i class="fa fa-star"></i></li>
-                  <li><i class="fa fa-star"></i></li>
-                </ul>
-                <span>Reviews (36)</span> -->
-                <div class="d-flex justify-content-end">
-                  <a href="http://" class="btn btn-info "> view</a>
-                </div>
-              </div>
-            </div>
-          </div>
+          <?php
+          endforeach;
+          
+        else:
+          echo $msg;
+        endif;
+          ?>
         </div>
       </div>
     </div>
--- viewPost.php
+++ viewPost.php
@@ -13,30 +13,56 @@
         </div>
       </div>
     </div>
-
+<?php
+require_once 'inc/dbconn.php';
+if (isset($_GET['id']))
+{
+  $id=$_GET['id'];
+}
+else
+{
+  header("location:index.php");
+}
+  $query="select * from posts where id=$id";
+  $runquey=mysqli_query($conn,$query);
+  
+  if (mysqli_num_rows($runquey)==1)
+  {
     
-    <div class="best-features about-features">
+    $post=mysqli_fetch_assoc($runquey);
+
+  }
+  else
+  {
+    $msg= "no posts founded";
+  }
+
+
+?>
+    
+<div class="best-features about-features">
       <div class="container">
         <div class="row">
           <div class="col-md-12">
             <div class="section-heading">
-              <h2>Our Background</h2>
+              <!-- <h2>Our Background</h2> -->
             </div>
           </div>
           <div class="col-md-6">
             <div class="right-image">
-              <img src="assets/images/feature-image.jpg" alt="">
+              <img src="assets/images/postImage/<?= $post['image'] ?>" alt="">
             </div>
           </div>
           <div class="col-md-6">
             <div class="left-content">
-              <h4>Who we are &amp; What we do?</h4>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed voluptate nihil eum consectetur similique? Consectetur, quod, incidunt, harum nisi dolores delectus reprehenderit voluptatem perferendis dicta dolorem non blanditiis ex fugiat. Lorem ipsum dolor sit amet, consectetur adipisicing elit.<br><br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, consequuntur, modi mollitia corporis ipsa voluptate corrupti eum ratione ex ea praesentium quibusdam? Aut, in eum facere corrupti necessitatibus perspiciatis quis.</p>
+              <h4><?= $post['title'] ?></h4>
+              <p><?= $post['body'] ?></p>
+              <h5><?= $post['created_at'] ?></h5><br>
               
               <div class="d-flex justify-content-center">
-                  <a href="editPost.php" class="btn btn-success mr-3 "> edit post</a>
+                  <a href="editPost.php?id=<?= $post['id']?>" class="btn btn-success mr-3 "> edit post</a>
               
-                  <a href="deletePost.php" class="btn btn-danger "> delete post</a>
+                  <a href="deletePost.php?id=<?= $post['id']?>" class="btn btn-danger "> delete post</a>
               </div>
             </div>
           </div>
